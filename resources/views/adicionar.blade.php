4d4cd88b85496620690c60df746a20f242e42bbb
lucaslopeslima95 <lucaslopesllima@gmail.com>
2023-11-22 20:11:18 -0300

    Ajustado template de salvar

--- resources/views/adicionar.blade.php
+++ resources/views/adicionar.blade.php
@@ -9,30 +9,31 @@
 </head>
 <body class="container-fluid vw-75">
   <h3>Adicionar Cep</h3>
-  <form >
+  <form action="{{  route('salvar') }}" method="POST">
+    @csrf
     <div class="mb-3">
       <label for="formGroupExampleInput" class="form-label">Cep:</label>
-      <input type="text" name="cep" class="form-control" id="formGroupExampleInput"  value="{{$cep}}" placeholder="CEP">
+      <input type="text" name="cep" class="form-control" id="formGroupExampleInput"  value="{{$cep}}" placeholder="CEP" required>
     </div>
     <div class="mb-3">
       <label for="formGroupExampleInput" class="form-label">Rua:</label>
-      <input type="text" name=" logradouro " class="form-control" id="formGroupExampleInput" value="{{ $logradouro }}"  placeholder="Rua">
+      <input type="text" name="logradouro" class="form-control" id="formGroupExampleInput" value="{{ $logradouro }}"  placeholder="Rua" required>
     </div>
     <div class="mb-3">
       <label for="formGroupExampleInput" class="form-label">Numero:</label>
-      <input type="text" name="numero" class="form-control" id="formGroupExampleInput" value=""  placeholder="Numero">
+      <input type="text" name="numero" class="form-control" id="formGroupExampleInput" value=""  placeholder="Numero" required>
     </div>
     <div class="mb-3">
       <label for="formGroupExampleInput" class="form-label">Bairro:</label>
-      <input type="text" name="bairro" class="form-control" id="formGroupExampleInput" value="{{$bairro}}" placeholder="Bairro">
+      <input type="text" name="bairro" class="form-control" id="formGroupExampleInput" value="{{$bairro}}" placeholder="Bairro" required>
     </div>
     <div class="mb-3">
       <label for="formGroupExampleInput" class="form-label">Cidade:</label>
-      <input type="text" name="cidade" class="form-control" id="formGroupExampleInput" value="{{$localidade}}" placeholder="Cidade">
+      <input type="text" name="cidade" class="form-control" id="formGroupExampleInput" value="{{$localidade}}" placeholder="Cidade" required>
     </div>
     <div class="mb-3">
       <label for="formGroupExampleInput" class="form-label">Estado:</label>
-      <input type="text" name="estado" class="form-control" id="formGroupExampleInput" value="{{$uf}}" placeholder="Estado">
+      <input type="text" name="estado" class="form-control" id="formGroupExampleInput" value="{{$uf}}" placeholder="Estado" required>
       <input type="submit" value="Salvar">
     </div>
   </form>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\EnderecoController;
 
 Route::get('/', [EnderecoController::class,'index'])->name('home');
 Route::get('/buscar', [EnderecoController::class,'buscar'])->name('buscar');
+Route::post('/salvar', [EnderecoController::class,'salvar'])->name('salvar');
